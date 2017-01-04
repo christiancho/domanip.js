778fe0ebdb01435fd4013c9ff8bcb306e90b57fb
christiancho <chochr@gmail.com>
2017-01-04 12:21:50 -0500

    Create fetching message

--- scripts/test_functions.js
+++ scripts/test_functions.js
@@ -1,14 +1,19 @@
 function changeGif(result){
   const info = JSON.parse(result).data;
+  $l('.giphy-box').removeClass('fetching');
   $l('.giphy-box').removeClass('unloaded');
   $l('.giphy-box').children().attr('src', info.image_original_url);
 }
 
 function randomGif(){
+  $l('.giphy-box').addClass('fetching');
+  $l('.giphy-box').children().attr('src', '');
   $l.ajax({
     url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg',
     success: (result) => changeGif(result)
-  }).then(console.log("AJAX call successful."));
+  }).then( () => {
+    console.log("AJAX call successful.");
+  });
 }
 
 function addBullet(){
